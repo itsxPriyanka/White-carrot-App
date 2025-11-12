dd925305d635d125860f9d614136d1fc408da299
Priyanka Kumari <nameispriyanka05@gmail.com>
2025-11-12 21:43:21 +0530

    Removed unused Navigate import to fix build error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
-
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import EditorPage from "./pages/EditorPage";
 import PreviewPage from "./pages/PreviewPage";
